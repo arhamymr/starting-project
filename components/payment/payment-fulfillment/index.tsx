a44d15213ffd4b22250b461087afec611f51f03a
muhammad arham <arhamymr@gmail.com>
2023-06-06 21:54:46 +0800

    update payment method

--- components/payment/payment-fulfillment/index.tsx
+++ components/payment/payment-fulfillment/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Text, Flex } from "@chakra-ui/react";
+import { Box, Text, Flex, Spinner, Center } from "@chakra-ui/react";
 import Image from "next/image";
 import { formatPrice, copyToClipboard } from "helpers/payment";
 import { CopyIcon } from "@chakra-ui/icons";
@@ -6,36 +6,41 @@ import Modal from "./modal";
 import ReactHtmlParser from "react-html-parser";
 import { useState } from "react";
 
-const content = [
-  // {
-  //   label: "Total Pembayaran",
-  //   type: "price",
-  //   value: 1549000,
-  // },
-  {
-    label: "Metode Pembayaran",
-    type: "image",
-    value: "/assets/payment/BRI_1685497356.png",
-  },
-  {
-    label: "Nomor Rekening",
-    type: "copy",
-    value: "005001005261301",
-  },
-  {
-    label: "Atas Nama",
-    type: "text",
-    value: "PT Serpul Teknologi Indonesia",
-  },
-];
-
+const content = (data) => {
+  return [
+    // {
+    //   label: "Total Pembayaran",
+    //   type: "price",
+    //   value: 1549000,
+    // },
+    {
+      label: "Metode Pembayaran",
+      type: "image",
+      value: "/assets/payment/BRI_1685497356.png",
+    },
+    {
+      label: "Nomor Rekening",
+      type: "copy",
+      value: data?.payment_method?.payment_number,
+    },
+    {
+      label: "Atas Nama",
+      type: "text",
+      value: data?.payment_method?.payment_owner,
+    },
+  ];
+};
 const ValueRender = ({ type, value }) => {
   const [copied, setCopied] = useState(false);
   switch (type) {
     case "text":
       return <Text>{value}</Text>;
     case "image":
-      return <Image src={value} alt={"value"} width={109} height={34} />;
+      return (
+        <Box mb={2}>
+          <Image src={value} alt={"value"} width={109} height={34} />
+        </Box>
+      );
     case "copy":
       return (
         <Flex alignItems={"center"} gap={3}>
@@ -65,62 +70,69 @@ const ValueRender = ({ type, value }) => {
 };
 
 export default function PaymentFulfilment({ data }) {
+  console.log(data);
   return (
     <Box>
-      <Box
-        borderRadius={"20px"}
-        flexDirection={"column"}
-        mb={"32px"}
-        bg={"gray.100"}
-        py={"28px"}
-        px={"41px"}
-        fontSize={"16px"}
-        fontWeight={600}
-      >
-        <Flex
-          mb={"16px"}
-          alignItems={"center"}
-          justifyContent={"space-between"}
-        >
-          <Text flex={1}>Total Pembayaran</Text>
-          <Box flex={1}>
-            <Text>Rp. {formatPrice(data?.total_price)}</Text>
-          </Box>
-        </Flex>
-        {content?.map((item, index) => (
-          <Flex
-            alignItems={"center"}
-            mb={index === content.length - 1 ? "0px" : "16px"}
-            key={item.label}
-            justifyContent={"space-between"}
+      {!data.total_price ? (
+        <Center h={40}>
+          <Spinner />
+        </Center>
+      ) : (
+        <Box>
+          <Box
+            borderRadius={"20px"}
+            flexDirection={"column"}
+            mb={"32px"}
+            bg={"gray.100"}
+            py={"28px"}
+            px={"41px"}
+            fontSize={"16px"}
+            fontWeight={600}
           >
-            <Text flex={1}>{item.label}</Text>
-            <Box flex={1}>
-              <ValueRender type={item.type} value={item.value} />
-            </Box>
-          </Flex>
-        ))}
-      </Box>
+            <Flex
+              mb={"16px"}
+              alignItems={"center"}
+              justifyContent={"space-between"}
+            >
+              <Text flex={1}>Total Pembayaran</Text>
+              <Box flex={1}>
+                <Text>Rp. {formatPrice(data?.total_price)}</Text>
+              </Box>
+            </Flex>
+            {content(data)?.map((item, index) => (
+              <Flex
+                alignItems={"center"}
+                mb={index === content.length - 1 ? "0px" : "16px"}
+                key={item.label}
+                justifyContent={"space-between"}
+              >
+                <Text flex={1}>{item.label}</Text>
+                <Box flex={1}>
+                  <ValueRender type={item.type} value={item.value} />
+                </Box>
+              </Flex>
+            ))}
+          </Box>
 
-      {/* <Text mb={"15px"} fontSize={"17px"} fontWeight={600}>
-        Petunjuk Pembayaran
-      </Text> */}
+          {/* <Text mb={"15px"} fontSize={"17px"} fontWeight={600}>
+      Petunjuk Pembayaran
+    </Text> */}
 
-      <Box px={5} mb={8}>
-        {ReactHtmlParser(
-          "<p><strong>Cara Pembayaran Melalui Bank Transfer (ATM) :</strong></p><ol><li>ATM BRI terdekat di kota anda.</li><li>Masukkan kartu ATM ke dalam mesin ATM seperti biasa.</li><li>Sesuaikan bahasa yang akan digunakan.</li><li>Masukkan nomor PIN ATM BRI anda.</li><li>Pilih Transaksi Lain.</li><li>Pilih Transfer.</li><li>Pilih BRI.</li><li>Masukkan nomor rekening BRI&nbsp;<strong>005001005261301 </strong>atas nama<strong> CV. Qiosfin Multipayment Indonesia</strong>, lalu pilih benar (jika rekening sudah benar).</li><li>Masukkan nominal transfer yang di minta (nominal transfer harus sesuai dengan yang di minta, termasuk 3 angka untuk di belakang nominal), pilih benar (pastikan jumlah nominal transfersudah benar).</li><li>Akan muncul informasi transfer, jika sudah benar selesaikan pembayaran anda.</li><li>Transfer Sukses.</li><li>Transfer Berhasil,&nbsp;selanjutnya konfirmasi pembayaran anda di detail deposit saldo larakost pulsa anda.</li><li>Saldo Qiosfin anda akan otomatis bertambah.</li></ol><p><strong>Cara Pembayaran Melalui Aplikasi BRIMO :</strong></p><ol><li>Buka Aplikasi Brimo</li><li>Masuk ke Menu “Transfer”</li><li>Pilih “Tambah Penerima”</li><li>Pilih Bank Tujuan “Bank BRI”</li><li>Masukkan nomor rekening BRI&nbsp;<strong>005001005261301 </strong>atas nama<strong> CV. Qiosfin Multipayment Indonesia</strong>, lalu pilih lanjutkan (jika rekening sudah benar).</li><li>Masukkan nominal transfer yang di minta (nominal transfer harus sesuai dengan yang di minta, termasuk 3 angka untuk di belakang nominal), pilih Transfer (pastikan jumlah nominal transfer sudah benar).</li><li>Transfer Sukses.</li><li>Transfer Berhasil,&nbsp;selanjutnya konfirmasi pembayaran anda di detail deposit saldo larakost pulsa anda.</li><li>Saldo Qiosfin anda akan otomatis bertambah.</li></ol><p><strong>Catatan Penting :</strong></p><ol><li>Deposit akan diproses secara otomatis oleh sistem jika dana sudah terbaca di mutasi rekening BRI.</li><li>Rata-rata proses deposit adalah kurang dari 15 menit (Pada Jam Normal Bank).</li><li>Deposit Saldo melalui Mesin EDC / BRILink akan H+1 (Keesokan harinya) setelah mengirimkan bukti pembayaran</li></ol>"
-        )}
-      </Box>
-      {/* <OrderedList mb={"28px"} fontSize={"13px"}>
-        {data.map((item, index) => {
-          return (
-            <>
-              <ListItem key={index}>{item}</ListItem>
-            </>
-          );
-        })}
-      </OrderedList> */}
-      <Modal />
+          <Box px={5} mb={8}>
+            {ReactHtmlParser(data?.payment_method?.how_to_use)}
+          </Box>
+          {/* <OrderedList mb={"28px"} fontSize={"13px"}>
+      {data.map((item, index) => {
+        return (
+          <>
+            <ListItem key={index}>{item}</ListItem>
+          </>
+        );
+      })}
+    </OrderedList> */}
+          <Modal />
+        </Box>
+      )}
     </Box>
   );
 }
